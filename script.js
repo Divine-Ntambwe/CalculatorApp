dd927b0edceb7370834b3c8b87e18c62454c42e7
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-06-07 21:05:37 +0200

    i think im done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // GLOBAL VARIABLES
 let num1;
-let sign;
+let sign = ""; //gets the value from the text within the class .operators buttons
 let num2;
 let display = false;
 
 function displayNum(num) {
   const screen = document.getElementById("screen");
-  screen.textContent += num //document.getElementById(num).textContent;
+  screen.textContent += num 
   display = true;
 }
 
@@ -16,6 +16,37 @@ function clearDisplay() {
   display = false
 }
 
+function displayAns(operator){
+  if (display){
+    let ans = 0;
+    const screen = document.getElementById("screen");
+    sign = document.getElementById(operator).textContent;
+    num1 = screen.textContent;
+    switch (sign) {
+      case "2/": ans = Math.sqrt(num1);
+      break;
+      case "^2": ans = num1**2;
+      break;
+    } 
+    screen.textContent = ans;
+  }
+}
+function percentage(){
+  const screen = document.getElementById("screen");
+  let newNum;
+  if (display) {
+    if (getNum2()) {
+      newNum = Number(num2)*0.01
+      num2 = 5 ;
+      screen.textContent = screen.textContent.slice(0,screen.textContent.lastIndexOf(num2.toString())) + newNum
+    } else {
+      num1 = screen.textContent;
+      newNum = Number(num1)*0.01
+      screen.textContent = newNum 
+      num1 = 5;
+    }
+  }
+}
 function operate(operator) {
   if (display) {
     if (getNum2()) {
@@ -26,28 +57,34 @@ function operate(operator) {
       num1 = screen.textContent;
       sign = document.getElementById(operator).textContent;
       screen.textContent += " " + document.getElementById(operator).textContent + " ";
+      display = false;
   }
-}
+} 
 
 function getNum2() {
   const screen = document.getElementById("screen");
   let screenText = screen.textContent;
-
-  if (screenText.indexOf(sign) == - 1) {
+  
+  if (screenText.indexOf(sign) == - 1 || screenText.indexOf(sign) == (screenText.length-2) || sign == "")  {
     return false;
   } else {
     num2 = screenText.slice(screenText.indexOf(sign) + 2, screenText.length);
     return true; 
   }
+  
 }
 
 function equalTo() {
+   
   if (display) {
     if (getNum2()) {
       const screen = document.getElementById("screen");
-      let ans;
-      num1 = Number(num1);
-      num2 = Number(num2);
+      let ans = 0;
+       console.log(num2)
+       console.log(num1)
+       num1 = Number(num1);
+       num2 = Number(num2);
+    
 
       switch (sign) {
         case "+" :   ans = num1 + num2;
@@ -62,20 +99,12 @@ function equalTo() {
         case "x": ans = num1*num2;
          break;
 
-        case "sqrx": ans = num1 ** num2;
+        case "^x": ans = num1 ** num2;
          break;
 
-      }
 
-      // if (sign === "+") {
-      //   ans = num1 + num2;
-      // } else if (sign === "-") {
-      //   ans = num1 - num2;
-      // } else if (sign === "/") {
-      //   ans = num1 / num2;
-      // } else if (sign === "x") {
-      //   ans = num1 * num2;
-      // }
+
+      }
 
       screen.textContent = ans;
     }
